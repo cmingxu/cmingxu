578f4118a8735916c7b995c0ee2034133872159e
xcm <cming.xu@gmail.com>
2010-09-20 15:00:59 +0800

    remove

---
+++ ruby/test.rb
@@ -0,0 +1,23 @@
+print "%s\n" % "dddd"
+
+at_exit{
+  print "\n%s  at exit\n" %  "EXITING"
+}
+
+l = lambda do 
+  puts "sssss"
+end
+
+p = proc do
+  puts "Proc"
+end
+
+p.call
+l.call
+
+File.open(__FILE__) do |f|
+  p f.readline
+end
+IO.foreach(__FILE__) do |line|
+  p line
+end
